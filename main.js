55b74e1b39c32a6b089c647cea8b817bc95709e4
baljinnym13 <baljinnym1318@gmail.com>
2024-06-30 13:11:42 +0800

    gert duusgaw

--- main.js
+++ main.js
@@ -1,60 +1,20 @@
 console.log("========");
 
-// document.getElementById("text").style.color = "red";
-// document.getElementById("text").style.fontSize = "40px";
-// document.getElementById("text").style.backgroundColor = "yellow";
-// // text utga oorchildog domuud
-// document.getElementById("text").textContent = "Pinecone LLC";
-// // text utga oorchildog zowhon text oruulna
-// document.getElementById("text").innerText = "Pinecone company";
-// // text , html utga oorchildog
-// document.getElementById("text").innerHTML = "Pinecone company html";
-// const myelement =
 console.log("Working");
-// const textEl = document.getElementById("text");
-// const myElement = document.createElement("h1");
-// myElement.textContent = "Pinecone LLC";
-// document.getElementById("test").appendChild(myElement);
-// console.log(myElement);
-// const els = document.getElementsByClassName("demo");
-// console.log("EL", els[0].children);
-// function handleClick() {
-//   console.log("Button clicked");
-
-//   textEl.style.color = "red";
-
-//   textEl.style.fontSize = "40px";
-
-//   textEl.style.backgroundColor = "yellow";
-
-//   textEl.textContent = "Pinecone LLC";
-
-//   textEl.innerText = "<h1>Pinecone Company</h1>";
-
-//   textEl.innerHTML = "<h1>Pinecone Company</h1>";
-// }
-
-// function change() {
-//   console.log("Button clicked");
-// }
-// const btnEl = document.getElementById("btn");
-// console.log("=====>", btnEl);
-// btnEl.addEventListener("click", change);
-
-// const boxEl1 = document.getElementsByClassName("box1")[0];
-// const boxEl2 = document.getElementsByClassName("box2")[0];
-// const boxEl3 = document.getElementsByClassName("box3")[0];
-// const boxEl4 = document.getElementsByClassName("box4")[0];
-// const boxEl5 = document.getElementsByClassName("box5")[0];
-// const boxEl6 = document.getElementsByClassName("box6")[0];
-// const boxEl7 = document.getElementsByClassName("box7")[0];
-// const boxEl8 = document.getElementsByClassName("box8")[0];
-// const boxEl9 = document.getElementsByClassName("box9")[0];
 const boxels = document.getElementsByClassName("box");
-const btnEl = document.getElementsByClassName("btn")[0];
-const colors = ["green", "yellow", "red", "blue", "gray"];
-let i = 0;
-
+const btnEl = document.getElementsByClassName("btnchange")[0];
+const mainbox = document.getElementById("main");
+const btnadd = document.getElementsByClassName("btnnemeh")[0];
+const btnhas = document.getElementsByClassName("btnhasah")[0];
+const newBox = document.createElement("div");
+btnadd.addEventListener("click", () => {
+  const newBox = document.createElement("div");
+  newBox.className = "box";
+  mainbox.appendChild(newBox);
+});
+btnhas.addEventListener("click", () => {
+  mainbox.removeChild;
+});
 function generatecolor() {
   const rcolor = Math.floor(Math.random() * 256);
   const gcolor = Math.floor(Math.random() * 256);
@@ -67,31 +27,5 @@ function changebackgroundcolor() {
   for (let i = 0; i < boxels.length; i++) {
     boxels[i].style.backgroundColor = generatecolor();
   }
-  // boxEl1.style.backgroundColor = generatecolor();
-  // boxEl2.style.backgroundColor = generatecolor();
-  // boxEl3.style.backgroundColor = generatecolor();
-  // boxEl4.style.backgroundColor = generatecolor();
-  // boxEl5.style.backgroundColor = generatecolor();
-  // boxEl6.style.backgroundColor = generatecolor();
-  // boxEl7.style.backgroundColor = generatecolor();
-  // boxEl8.style.backgroundColor = generatecolor();
-  // boxEl9.style.backgroundColor = generatecolor();
-  // const rcolor = Math.floor(Math.random() * 256);
-  // const gcolor = Math.floor(Math.random() * 256);
-  // const bcolor = Math.floor(Math.random() * 256);
-  // boxEl.style.backgroundColor =
-  //   "rgb(" + rcolor + ", " + gcolor + "," + bcolor + ") ";
-  // boxEl.style.backgroundColor = colors[i];
-  // i++;
-  // if (i === colors.length) {
-  //   i = 0;
-  // }
 }
-
 btnEl.addEventListener("click", changebackgroundcolor);
-const playbtn = document.getElementById("playbtn");
-playbtn.addEventListener("click", () => {
-  const audio = document.getElementsByTagName("audio")[0];
-  audio.play;
-  audio.src = "";
-});
